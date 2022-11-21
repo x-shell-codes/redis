x-shell-codes/swap

Vagrantfile added in project.

Vagrantfile
@@ -1,0 +1,9 @@
+Vagrant.configure("2") do |config|
+	config.vm.box = "ubuntu/jammy64"
+
+	config.vm.provider "virtualbox" do |vb|
+		vb.name = "swap.local.x-shell.codes"
+		vb.cpus = 1
+		vb.memory = 4096
+	end
+end

x-shell-codes/redis

Vagrantfile
@@ -1,0 +1,9 @@
+Vagrant.configure("2") do |config|
+	config.vm.box = "ubuntu/jammy64"
+
+	config.vm.provider "virtualbox" do |vb|
+		vb.name = "redis.local.x-shell.codes"
+		vb.cpus = 1
+		vb.memory = 4096
+	end
+end